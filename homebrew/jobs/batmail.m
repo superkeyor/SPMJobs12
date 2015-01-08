95b54e53b40b1302148804b1adde09fdb6138e7f
Jerry Zhu <jerryzhujian9@gmail.com>
2015-01-07 18:53:42 -0600

    batmail

---
+++ homebrew/jobs/batmail.m
@@ -0,0 +1,5 @@
+function main(funcName, startTime, finishTime)
+    
+    ez.gmail('jerryzhu@siu.edu', ['SPM ' funcName ' at ' finishTime], startTime, ...
+            'Memory Lab <fmrimemorylab@gmail.com>', 'fmrimemorylab@gmail.com', 'vviwoovwrhyljbsz');
+end
