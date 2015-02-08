4a59145da5f393e4a170ee3f0edc713f8ed7d89c
Jerry Zhu <jerryzhujian9@gmail.com>
2015-02-08 00:12:27 -0600

    gogogo

---
+++ template/gogogo.m
@@ -0,0 +1,6 @@
+run('C:\Users\Administrator\Desktop\AA2Jerry\01Import\go')
+run('C:\Users\Administrator\Desktop\AA2Jerry\02Concat\go')
+run('C:\Users\Administrator\Desktop\AA2Jerry\03SliceTiming\go')
+run('C:\Users\Administrator\Desktop\AA2Jerry\04Motion\go')
+run('C:\Users\Administrator\Desktop\AA2Jerry\05Warp2Norm\go')
+run('C:\Users\Administrator\Desktop\AA2Jerry\06Smooth\go')
