8506d779290e7b7693714b157bc1326c20745b8e
Jerry Zhu <jerryzhujian9@gmail.com>
2015-12-05 01:01:07 -0500

    delete spms

--- spms/extensions/decoding_toolbox_v3.51/access_images/spm2/get_filenames_spm2.m
+++
@@ -1,7 +0,0 @@
-function varargout = get_filenames_spm2(varargin)
-
-%   get_filenames:
-%       inputs: file path (1 x n string), possible filenames with wildcards (e.g. *.img or rf*.img)
-%       output: filenames as n x m char array (n = number of files)
-
-varargout{1} = spm_get('Files',varargin{1},varargin{2});
