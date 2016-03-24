721b9672e64d533a6374b3794d5f80c26e033eca
Jerry Zhu <jerryzhujian9@gmail.com>
2016-03-24 15:45:55 -0400

    whereami

--- homebrew/easylazy/whereami.m
+++ homebrew/easylazy/whereami.m
@@ -1,15 +1,15 @@
 % Find anatomical location of a list of mni coordinates
 % example: 
-% mni([20 6 10; 30 9 12])
-% mni([20 6 10])
-% mni [20 6 10]
+% ([20 6 10; 30 9 12])
+% ([20 6 10])
+%  [20 6 10]
 % returns nothing
 % also: check out Neurosynth (http://neurosynth.org)
 
 % wrapper of http://www.alivelearn.net/?p=1456 
 
 function main(varargin)
-    % if mni [20 6 10]
+    % if  [20 6 10]
     if iscellstr(varargin); varargin = {str2num(varargin{:})}; end;
     
     % [oneline, cellarray]=cuixuFindStructure([20 6 10; 30 9 12])
