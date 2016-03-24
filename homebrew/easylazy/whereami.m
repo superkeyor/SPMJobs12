c12659d53b2bc9f8507de538c728ca1405c7487b
Jerry Zhu <jerryzhujian9@gmail.com>
2016-03-24 17:43:15 -0400

    whereami

--- homebrew/easylazy/whereami.m
+++ homebrew/easylazy/whereami.m
@@ -2,10 +2,10 @@
 % example: 
 % ([20 6 10; 30 9 12])
 % ([20 6 10])
-%  [20 6 10]
+% command line: [20 6 10]
 % returns nothing
-% also: check out Neurosynth (http://neurosynth.org)
 
+% also: check out Neurosynth (http://neurosynth.org)
 % wrapper of http://www.alivelearn.net/?p=1456 
 
 function main(varargin)
@@ -21,6 +21,9 @@ function main(varargin)
         % varargin{1} is the passed matrix
         disp(varargin{1}(i,:));
         disp(oneline{i});
+
+        url = sprintf('http://neurosynth.org/locations/%d_%d_%d/',varargin{1}(i,1),varargin{1}(i,2),varargin{1}(i,3));
+        disp(sprintf('\n<a href="%s">%s</a>',url,url));
     end
     
 end
