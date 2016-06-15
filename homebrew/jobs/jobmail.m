f978f6cb7a589f1c8413711981901fed7e6bba01
Jerry Zhu <jerryzhujian9@gmail.com>
2016-06-15 10:27:57 -0400

    jobmail

--- homebrew/jobs/jobmail.m
+++
@@ -1,5 +0,0 @@
-function main(funcName, startTime, finishTime)
-    
-    ez.gmail('jerryzhu@siu.edu', [funcName ' at ' finishTime], startTime, ...
-            'Memory Lab <fmrimemorylab@gmail.com>', 'fmrimemorylab@gmail.com', 'vviwoovwrhyljbsz');
-end
