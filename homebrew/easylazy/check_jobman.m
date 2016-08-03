016dd0bae7dce63703a26e00d31ddd35806df4dc
Jerry Zhu <jerryzhujian9@gmail.com>
2016-08-03 14:59:18 -0400

    check job spm 5

--- homebrew/easylazy/check_jobman.m
+++ homebrew/easylazy/check_jobman.m
@@ -1,5 +1,9 @@
-function varargout = main(varargin)
+function varargout = main(j)
     % load job into Batch GUI
-    matlabbatch = evalin('base', 'matlabbatch');
+    % j default 'matlabbatch', in case spm 5 ('jobs')
+    % spm 8/12 use matlabbatch, spm 5 uses jobs
+
+    if nargin<1, j = 'matlabbatch'; end
+    matlabbatch = evalin('base', j);
     spm_jobman('interactive',matlabbatch);
 end
