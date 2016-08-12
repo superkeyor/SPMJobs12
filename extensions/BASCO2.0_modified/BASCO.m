e1bef563009020f0bda8725efe25a5fdc3479d52
Jerry Zhu <jerryzhujian9@gmail.com>
2016-08-12 17:25:23 -0400

    basco

--- extensions/BASCO2.0_modified/BASCO.m
+++ extensions/BASCO2.0_modified/BASCO.m
@@ -600,7 +600,7 @@ for isubj=1:handles.NumJobs
     handles.InfoText = WriteInfoBox(handles,'Retrieving beta-values ...',true);
     SPMfile = fullfile(beta_path,'SPM.mat');
     ROISummaryFunction = handles.anaobj{isubj}.Ana{1}.AnaDef.ROISummaryFunction;
-    bs = GetROIBetaSeries(SPMfile,ROIfile,ROISummaryFunction);
+    bs = GetROIBetaSeries(SPMfile,ROIFile,ROISummaryFunction);
     handles.anaobj{isubj}.Ana{1}.BetaSeries  = bs;  % rows: beta-value and columns: ROIs
     disp('... done.');
     % correlation matrix
