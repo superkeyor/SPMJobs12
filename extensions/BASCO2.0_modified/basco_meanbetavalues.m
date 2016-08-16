8b2e1b0b9fb1649b65f7a21bb66d008e71cf0845
Jerry Zhu <jerryzhujian9@gmail.com>
2016-08-15 20:10:52 -0400

    buggy basic still buggy

--- extensions/BASCO2.0_modified/basco_meanbetavalues.m
+++ extensions/BASCO2.0_modified/basco_meanbetavalues.m
@@ -144,3 +144,39 @@ if viewer>0
   % store information to mat-file
   save('mean_beta.mat','pval','ROIFiles');
 end
+
+
+
+
+
+function theindices = Condition2Indices(anaobj,thecond)
+theindices = [];
+NumCond    = length(thecond);
+for icond=1:NumCond
+    theindices = [theindices anaobj.Ana{1}.AnaDef.RegCondVec{thecond(icond)}];
+end
+
+function newbs = CondSelBS(anaobj,thecond,bs)
+hrfderivs  = anaobj.Ana{1}.AnaDef.HRFDERIVS; % regressors for temporal derivatives?
+theindices = Condition2Indices(anaobj,thecond);
+if hrfderivs(1)==0 && hrfderivs(2)==0
+    newbs = bs(theindices);
+end
+if hrfderivs(1)==1 && hrfderivs(2)==0
+    X = anaobj.Ana{1}.AnaDef.X; % design (runs,regressors)
+    for itrial=1:length(theindices)
+        idx   = theindices(itrial);
+        beta1 = bs(idx);
+        beta2 = bs(idx+1);
+        newbs(itrial) = sign(beta1)*sqrt(beta1^2+beta2^2);
+    end
+end
+if hrfderivs(1)==1 && hrfderivs(2)==1 % fix me!
+    X = anaobj.Ana{1}.AnaDef.X; % design (runs,regressors)
+    for itrial=1:length(theindices)
+        idx   = theindices(itrial);
+        beta1 = bs(idx);
+        beta2 = bs(idx+1);
+        newbs(itrial) = sign(beta1)*sqrt(beta1^2+beta2^2);
+    end
+end
