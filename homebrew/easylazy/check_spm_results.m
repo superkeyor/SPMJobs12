127150c0d36fd640992b90122cfc5016134ad88e
Jerry Zhu <jerryzhujian9@gmail.com>
2016-09-12 16:25:34 -0400

    check

--- homebrew/easylazy/check_spm_results.m
+++ homebrew/easylazy/check_spm_results.m
@@ -1,9 +1,11 @@
-function varargout = main(varargin)
-    % check results, alias of spm_results_ui
-    
-    [varargout{1:nargout}] = spm_results_ui(varargin{:}); 
-end
+% function varargout = main(varargin)
+%     % check results, alias of spm_results_ui
+%     
+%     [varargout{1:nargout}] = spm_results_ui(varargin{:}); 
+% end
+% 
 
 
-
-
+% use guide to open spm_Menu.fig to see the callback
+[hReg,xSPM,SPM] = spm_results_ui;
+TabDat = spm_list('List',xSPM,hReg);
