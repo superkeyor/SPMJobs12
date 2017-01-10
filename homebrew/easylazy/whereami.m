9ddba959102365fe5d997fe66aed6d4293a11b83
Jerry Zhu <jerryzhujian9@gmail.com>
2017-01-09 23:47:51 -0500

    whereami

--- homebrew/easylazy/whereami.m
+++ homebrew/easylazy/whereami.m
@@ -110,7 +110,8 @@ function main(varargin)
             
             resultFile = sprintf('TabDat_%s.csv',evalin('base','xSPM.title'));
             ez.cell2csv(resultFile,result);
-            ez.pprint(['Done! Check ' resultFile '. Sort by Lobe then Hemisphere. BA refers to BA of nearest grey matter if not found. AAL labeling should be less weighted(?).']);
+            ez.pprint(['Done! Check ' resultFile '. Sort by Lobe then Hemisphere. BA refers to BA of nearest grey matter if not found.']);
+            ez.pprint('AAL labeling should be less weighted(?). One should also look at the activation map to see where the cluster is.');
             return
         catch
             ez.pprint('Something wrong. Did you extract table data structure from SPM results table to get ''TabDat''?');
