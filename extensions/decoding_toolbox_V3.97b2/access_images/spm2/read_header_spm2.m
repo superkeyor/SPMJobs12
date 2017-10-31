3139818ea85d449d7c17188680426a3aff7638ba
Jerry Zhu <jerryzhujian9@gmail.com>
2017-10-31 18:24:30 -0500

    delete mvps, spam, add fitv2

--- extensions/decoding_toolbox_V3.97b2/access_images/spm2/read_header_spm2.m
+++
@@ -1,7 +0,0 @@
-function varargout = read_header(varargin)
-
-%   read_header: 
-%       input: name of volume (full path , 1 x n string)
-%       output: header of volume
-
-varargout{1} = spm_vol(varargin{:});
--- extensions/decoding_toolbox_V3.97b2/demos/demo10_prevalenceInference.m
+++
@@ -1,8 +0,0 @@
-% This function tells you the location for demos for prevalence_inference.
-% You should find them in decoding_toolbox/statistics/prevalence_inference
-
-if isempty(which('decoding_defaults')), error('Please add TDT'), end
-
-display(['See demos in' char(10) ...
-    '   ' fileparts(which('prevalenceTDT')) char(10) ...
-    'on how to use the prevalence inference analysis'])
