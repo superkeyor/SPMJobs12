76c3e783a906dd2870f2d7991e248bda9c85e286
Jerry Zhu <jerryzhujian9@gmail.com>
2018-12-07 23:52:26 -0500

    update

--- homebrew/easylazy/do_make_covariates_2ndlevel.m
+++ homebrew/easylazy/do_make_covariates_2ndlevel.m
@@ -4,7 +4,7 @@ function varargout = main(df, idcol, ids, covars, matname)
     % df: table, like df in R
     % idcol: col name as unique id to match rows
     % ids: a cell array containing ids in certain order
-    % covars: a cell array containing col names as covariates (not include idcol)
+    % covars: a cell array containing col names as covariates (not include idcol), if {''}, no file will be generated
     % matname: path to covariate mat, default covar2ndlvl.mat in pwd
     % 
     % example, typical usage: 
@@ -13,7 +13,10 @@ function varargout = main(df, idcol, ids, covars, matname)
     % MCI = nbk(strcmp(nbk.dx,'MCI'),:);
     % ids = [CN.linkid; SCD.linkid; MCI.linkid];
     % do_make_covariates_2ndlevel(nbk,'linkid',ids,{'ageMRI','sex','educ'});
-
+    
+    % no file will be generated
+    if strcmp(covars{1},''), return; end
+    
     if nargin==4, matname='covar2ndlvl.mat'; end
 
     % https://www.mathworks.com/matlabcentral/answers/22926-finding-the-indices-of-the-elements-of-one-array-in-another
