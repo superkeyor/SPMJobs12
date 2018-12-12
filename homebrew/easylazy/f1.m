bbaa9e859be6f87f5906ca48cf6a49c9950ba96a
Jerry Zhu <jerryzhujian9@gmail.com>
2018-12-12 11:14:04 -0500

    update

--- homebrew/easylazy/f1.m
+++ homebrew/easylazy/f1.m
@@ -1,12 +1,14 @@
 function main()
     % toggle spm and xjview windows on top
     spmWins = findall(0,'type','figure','-regexp','name','^SPM');
-    for w = 1:numel(spmWins)
-        ez.WinTop(spmWins(w));
+    for i = 1:numel(spmWins)
+        w = spmWins(i);
+        if strcmp(w.Visible,'on'), ez.WinTop(w); end
     end
 
     xjWins = findall(0,'type','figure','-regexp','name','^xjView');
-    for w = 1:numel(xjWins)
-        ez.WinTop(xjWins(w));
+    for j = 1:numel(xjWins)
+        w = xjWins(j);
+        if strcmp(w.Visible,'on'), ez.WinTop(w); end
     end
 end
