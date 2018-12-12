f14b1acc28329e3804e45fb916c6e7951ec88cd8
Jerry Zhu <jerryzhujian9@gmail.com>
2018-12-12 16:08:07 -0500

    update

--- homebrew/easylazy/check_spm_results_batch.m
+++ homebrew/easylazy/check_spm_results_batch.m
@@ -25,6 +25,7 @@ function varargout = main(maps)
             if strcmp(w.Visible,'on'), ez.WinTop(w); end
         end
         
+        pause(1.5);
         input(sprintf('%d of %d -- Press Enter key to move on.', i, numel(maps)));
     end
 end
