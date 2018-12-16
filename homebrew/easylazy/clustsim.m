22d2aee304f9711ccce597eaef87bbeb2ce7a945
Jerry Zhu <jerryzhujian9@gmail.com>
2018-12-16 14:27:41 -0500

    update

--- homebrew/easylazy/clustsim.m
+++ homebrew/easylazy/clustsim.m
@@ -1,71 +1,40 @@
-function clustsim(path, pthr, athr, extras, open_file)
-% FORMAT clustsim(path, pthr, athr, extras, open_file)
-% Runs AFNI's 3dClustSim (Monte Carlo simulations) to determine optimal 
-% cluster size threshold for second-level whole brain results.
-% Uses 3dcalc to create sqrt_ResMS image, then uses 3dFWHMx to obtain
-% smoothness of the noise in x, y, and z dimensions. Finally, it runs
-% 3dClustSim (10000 iterations) to get a table of minimum cluster sizes for
-% different p and alpha thresholds. 
+function main(ResMSPath)
+%DESCRIPTION:
+%    3dFWHMx + 3dClustSim
 %
+% USAGE:
+%    (ResMSPath)
 %
-% path:             Path to folder containing second level SPM.mat.
-%                   String.
-% pthr:             Optional. P threshold(s) at which clustsim will be run. 
-%                   Double vector.
-% athr:             Optional. Alpha value(s) for which clustsim will 
-%                   present results. Double vector.
-% extras:           Optional. Additional inputs (e.g. -nodec -2sided
-%                   -quiet). String.
-% open_file:        Optional. Chooses whether or not to open the
-%                   results file at the end. 1 to open file, 0 to not.
-    
-if ~exist(path,'dir')
-    fprintf('Directory does not exist: %s\n',path);
-    return
+% INPUT:
+%     ResMSPath: default={'ResMS.nii'}, cell str of paths
+%
+% OUTPUT:
+%     put generated files in auto-created folder clustsim
+%     printout results NN3_2-sided
+%     if NN3_2-sided result exists in the folder, only printout
+    if strcmp(ResMSPath,'-h'), ez.showhelp(); return; end
+    ez.setdefault({'ResMSPath', {'ResMS.nii'});
+    oldpwd = pwd;
+    for i = 1:numel(ResMSPath)
+        residual = ResMSPath{i};
+        ez.cd(ez.splitpath(ez.abspath(residual))); 
+        ez.mkdir('clustsim');
+        ez.cd('clustsim');
+
+        if ~ez.exists('3dClustSim.NN3_2sided.1D')
+            cmd = '3dcalc -a ../ResMS.nii -expr ''sqrt(a)'' -prefix sqrt_ResMS.nii';
+            ez.execute(cmd,0);
+            cmd = '3dFWHMx -mask ../mask.nii -input sqrt_ResMS.nii';
+            [status,result] = ez.execute(cmd,0);
+            result = strsplit(result,'\n');
+            result = result{7};
+            result = ez.trim(result); result = strsplit(result,' ');
+            ez.print(sprintf('3dFWHMx: %s',strjoin(result)));
+            cmd = sprintf('3dClustSim -mask ../mask.nii  -acf %s %s %s -iter 10000 -nodec -prefix 3dClustSim',result{1},result{2},result{3});
+            ez.execute(cmd,0);
+        end 
+        
+        lines = ez.readlines('3dClustSim.NN3_2sided.1D');
+        line = lines{end-3}; line = ez.trim(line); line = strsplit(line,' ');
+        ez.pprint(sprintf('0.05 <-- pthr = %s, k = %s', regexprep(line{1},'0*$',''), line{3}));
 end
-
-if ~exist('pthr','var')
-    fprintf('pthr is empty, setting to default\n');
-    pthr = [0.05 0.01 0.005 0.001];
-    %pthr = [0.001];
-elseif ischar(pthr)
-    fprintf('pthr is string, setting to default\n');
-    pthr = [0.05 0.01 0.005 0.001];
-end
-
-if ~exist('athr','var')
-    fprintf('athr is empty, setting to default\n');
-    athr = 0.05;
-elseif ischar(athr)
-    fprintf('athr is string, setting to default\n');
-    athr = 0.05;
-end
-
-if ~exist('extras','var')
-    extras = '';
-end
-
-if ~exist(open_file, 'var')
-    open_file = 0;
-end
-
-system(['/nfs/pkg64/afni/3dcalc -a ' path '/ResMS.hdr -expr "sqrt(a)" -prefix ' path '/sqrt_ResMS.nii -overwrite']);
-system(['/nfs/pkg64/afni/3dFWHMx -mask ' path '/mask.hdr -input ' path '/sqrt_ResMS.nii -output ' path '/fwhm.txt -overwrite']);
-fid = fopen([path '/fwhm.txt']);
-fwhm = textscan(fid,'%f');
-fwhmx = fwhm{1}(1);
-fwhmy = fwhm{1}(2);
-fwhmz = fwhm{1}(3);
-
-delete([path '/fwhm.txt']);
-
-system(['/nfs/pkg64/afni/3dClustSim -mask ' path '/mask.hdr -iter 10000 -pthr ' num2str(pthr) ' -athr ' num2str(athr) ' -fwhmxyz ' ... 
-    num2str(fwhmx) ' ' num2str(fwhmy) ' ' num2str(fwhmz) ' ' extras ' -prefix ' path '/montecarlo']);
-
-if open_file == 1
-    system(['emacs22-gtk ' path '/montecarlo.NN1.1D']);
-else
-    fprintf('open_file is not 1, not opening output file.\n');
-end
-end
-
